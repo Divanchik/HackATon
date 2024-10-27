3d4027360ffecd796d5803f48546a8d92ca56b51
Pavel Kazantsev <pkazantsev@syntellect.ru>
2024-10-27 12:41:36 +0400

    upd

--- DataCraftServer/Program.cs
+++ DataCraftServer/Program.cs
@@ -3,9 +3,7 @@ using DataCraftServer.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+
 builder.Services.AddScoped<IPostgreSQLService, PostgreSQLService>();
 
 var connectionString = builder.Configuration["DbConnection"];
@@ -16,11 +14,32 @@ builder.Services.AddDbContext<ApplicationContext>((sp, options) =>
     options.UseNpgsql(connectionString)
 );
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", b => b
+           //.AllowAnyOrigin()
+           //.SetIsOriginAllowed(host => true)
+           .WithOrigins("http://localhost:3000", "http://127.0.0.1:5500", "http://127.0.0.1:5173")
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .AllowCredentials());
+
+});
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
 var app = builder.Build();
+app.UseCors("CorsPolicy");
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapGet("/", (ApplicationContext db) => db.Users.ToList());
 app.UseRouting();
 app.MapControllers();
+
+
 
 app.Run();
